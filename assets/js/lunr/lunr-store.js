667d1f0dcd743425fa220745f1166dbef0ff19a4
wildone <wildone@users.noreply.github.com>
2020-07-17 17:34:53 +0000

    deploy: efa1971dafdae7c13214d66a68ceff939251983e

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -17,6 +17,78 @@ var store = [{
         "url": "https://governance.foundation/frameworks/pragmaticea",
         "teaser": null
       },{
+        "title": "Governance",
+        "excerpt":"These pages are dedicated to provide foundation aknowledge about concepts of Governance. Table of Contents Introduction - What is Governance? Governance Origin - Origin of the word Governance. Governance Paradigm Governance Evolution Governance Perspectives - What are some Governance Perspectives? Global Governance Modern Governance Corporate Governance Enterprise Governance IT Governance...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/",
+        "teaser": null
+      },{
+        "title": "Introduction",
+        "excerpt":"On This Page What is Governance Terms The System The Social Self-organisation The Organisation The Information System What is Governance Governance described and detailed as a conceptual perspective of a particular domain and its context, it’s aimed at unambiguously conveying a context of an entity within its domain that contributes...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/introduction",
+        "teaser": null
+      },{
+        "title": "Governance Origin",
+        "excerpt":"The term ‘governance’ has fallen into disuse (Hufty, 2009), it originates from a Greek verb ‘kubernân’ which refers to the verb ‘pilot’ (Oliveira, 2003) in reference to ‘pilot a ship’. The Greek term subsequently gave birth to the Latin verb ‘gubernare’ of similar meaning; its derivative ‘gubernantia’, in turn, gave...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/origin",
+        "teaser": null
+      },{
+        "title": "Governance Paradigm",
+        "excerpt":"The Greek reference to the term governance and its subsequent translations are not conclusive to the exact meaning of the term. Attempts at distinction, although evident in subsequent translations, are not authoritative due to lack of consistency in the definitions. A prominent distinction was made by Plato who used the...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/paradigm",
+        "teaser": null
+      },{
+        "title": "Governance Evolution",
+        "excerpt":"The evolution of the term governance in its many perspectives is being driven by the need to establish a predictive and repeatable governance norm. Efforts at clarification to date have been a multitude of disparate normative and prescriptive frameworks, and a governance norm still does not exist despite the unprecedented...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/evolution",
+        "teaser": null
+      },{
+        "title": "Governance Perspectives",
+        "excerpt":"Despite having no universal definition for the term governance, governance concept is used as a perspective in many fields (Hufty, 2009). This section will highlight some of the existing perspectives of governance, all of which are well-known governance concepts describing governance at different levels of society. Although these perspectives could...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/perspectives",
+        "teaser": null
+      },{
+        "title": "Global Governance",
+        "excerpt":"Hufty describes Global Governance as a concept for establishing order in international regimes (Hufty, 2009). The concept emerged due to the absence of a supra-state entity in the international systems that could arbitrate conflict between actors. According to Hufty, the complexity in the number of participating actors and the institutionalisation...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/global",
+        "teaser": null
+      },{
+        "title": "Modern Governance",
+        "excerpt":"Modern governance refers to the concept of the State being dependant on heterarchical regulation mechanisms, caused by the loss or delegation of its power and competencies, to various local, national and international entities. This concept is used as an instrument for analysis and action, contributing to the solution of the...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/modern",
+        "teaser": null
+      },{
+        "title": "Corporate Governance",
+        "excerpt":"Recent attention of academics and practitioners on Corporate Governance has emerged due to widely publicised scandals around the world involving corporations and, in particular, the conduct of Boards of Directors in large corporations (Cross, 2004; Colley, 2005). These scandals led to the development of new regulations and legislation, the most...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/corporate",
+        "teaser": null
+      },{
+        "title": "Enterprise Governance",
+        "excerpt":"ITGI used the term “Enterprise Governance” to describe a globally applicable prescribed model of organisational governance called the “Enterprise Governance Model” (ITGI, 1999). The Enterprise Governance Model was created in response to three environmental threats listed below as identified by the Information Systems Audit and Control Foundation (ISACF): The ease...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/enterprise",
+        "teaser": null
+      },{
+        "title": "Information Technology Governance",
+        "excerpt":"The term Information Technology Governance (IT Governance), represents a functional governance model fundamental to an IT organisation. It is part of the overall corporate governance structure (Brown &amp; Grant, 2005) and applies principals similar to financial governance to IT Management (Weill &amp; Woodham, 2002). Recently the topic of IT Governance...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/information-technology",
+        "teaser": null
+      },{
+        "title": "Systemic Governance",
+        "excerpt":"Meyer stipulates that an alternative to governance through oversight is systemic governance, implemented by replacing oversight controls with systemic controls to induce desired behaviours (Meyer, 2004). Meyer further associates the systemic controls with forces prevalent in the market economy, forces that align every business with the needs of its clients...","categories": ["governance","framework"],
+        "tags": ["tools"],
+        "url": "https://governance.foundation/governance/systemic",
+        "teaser": null
+      },{
         "title": "Governance Foundation",
         "excerpt":"Welcome to Governance Foundation!   This site will aim to collate relevant knowledge that provides a foundation for developing governance frameworks.   As well as providing insight on available governance frameworks, a new concept’s for governance will be explored.   Welcome to the journey!  ","categories": ["blog"],
         "tags": ["governance","blog"],
@@ -30,7 +102,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Governance Experience Platform",
-        "excerpt":"In organisations miscommunication is pathological, and it is prevalent in all types of socio-technical organisations startups, government and corporates. The primary reason for this is audience conflict, where teams loyalty at lower levels often erode its ability to deal with external ideas, other groups and its allegiance to the organisation...","categories": ["blog"],
+        "excerpt":"On This Page Pathology of Organisational Miscommunication Sharing Knowledge in Socio-Technical Organisations Organisational Deafness Governance Reborn Beyond Governance There is a clear need for a new way to help to govern organisations. This need is apparent from the continual search for efficiencies and effectiveness of organisations which demonstrated through transformations...","categories": ["blog"],
         "tags": ["governance","blog"],
         "url": "https://governance.foundation/blog/2020/07/15/governance-experience-platform",
         "teaser": null
