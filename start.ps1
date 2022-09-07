0d8d8609256ad083913fbb664b8d0b7a7d253f16
wildone <max.barrass@gmail.com>
2022-09-07 19:19:37 +1000

    update to latest docker config.

--- start.ps1
+++ start.ps1
@@ -1,16 +1,40 @@
-Write-Host "Starting Docker container to run build server"
+Param(
+  [string]$LOG_PATH = ".\logs",
+  [string]$LOG_PEFIX = "start",
+  [string]$LOG_SUFFIX = ".log",
+  [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
+  [string]$SERVICES = "$( (Get-Content ".\start-services.conf" | Where-Object {$_ -notmatch '^#.*'} | Where-Object {-not([String]::IsNullOrWhiteSpace($_))} ) -join " --file ")",
+  [string]$PORT = "443",
+  [Parameter(Position=0)]
+  [string]$SERVICE_NAME = "" # services to start
+)
 
-$SITE_NAME = Get-Content .\CNAME -Raw
-$SITE_NAME = $SITE_NAME -replace "[.\n\r]", "-"
-$CONATINER_NAME = "build-$SITE_NAME"
+$SKIP_CONFIG = $true
+$PARENT_PROJECT_PATH = "."
 
-$PORT = Get-Content .\dev.port -Raw
-$PORT = $PORT -replace "[.\n\r]", ""
-$PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
-$PORT_LIVERELOAD = $PORT_LIVERELOAD -replace "[.\n\r]", ""
+. ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
 
-#start docker build container
-docker run --name $CONATINER_NAME -d -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack sleep inf
 
-#connect to container
-docker exec -it $CONATINER_NAME bash --login
+# get env config
+$CONFIG_ENV = (Get-Content ".env" -Raw | ConvertFrom-StringData)
+$PORT=$CONFIG_ENV.TRAEFIK_HTTP_PORT
+
+$DOCKER_COMMAND="docker-compose --env-file .env --file docker-compose.yml --file ${SERVICES} up ${SERVICE_NAME}"
+
+if ( [string]::IsNullOrEmpty(${SERVICE_NAME}) ) {
+
+  $OPEN_COMMAND="explorer https://localhost"
+
+  printSubSectionStart "Open Console at https://localhost"
+
+  Invoke-Expression "${OPEN_COMMAND}"
+
+}
+
+printSubSectionStart "Docker Compose Execute"
+
+printSectionLine "${DOCKER_COMMAND}"
+
+Invoke-Expression "${DOCKER_COMMAND}"
+
+
